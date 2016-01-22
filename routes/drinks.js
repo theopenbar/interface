ca420126e23321ecfb4b1446c7c25abdfc4f82c9
Dillon <Dillon@SaraAndrsonsMBP.home>
2016-01-22 17:36:47 -0500

    use jade to display drinks json

--- routes/drinks.js
+++ routes/drinks.js
@@ -28,20 +28,11 @@ router.get('/', function(req, res) {
     // Let's find all the documents
     DrinksModel.find({}).exec(function(err, result) {
         if (!err) {
-            res.end(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
+            res.render('drinks', {json: JSON.stringify(result, undefined, 2), length: result.length})
         } else {
             res.end('Error in first query. ' + err)
         };
     });
 });
-
-//
-// The rudimentary HTML content in three pieces.
-var html1 = '<head> \
-<style> body {color: #394a5f; font-family: sans-serif} </style> \
-</head> \
-<body> \
-<h2> All Documents in MonogoDB database </h2> <pre><code> ';var html2 = '</code></pre> <br\> <i>';
-var html3 = ' documents. </i>';
 
 module.exports = router;
