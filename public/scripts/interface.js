26a6536b2a572f86b7af7116fb4b5ac2cdf9b119
Dillon <Dillon@SaraAndrsonsMBP.home>
2016-01-23 17:10:45 -0500

    implement the drink controller

--- public/scripts/interface.js
+++ public/scripts/interface.js
@@ -1,11 +1,19 @@
-var app = angular.module('interface', ['ngRoute']);
+var app = angular.module('interface', ['ngResource', 'ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'partials/home.html'
+            controller: 'HomeCtrl'
         })
         .otherwise({
             redirectTo: '/'
         });
+}]);
+
+app.controller('HomeCtrl', ['$scope', '$resource', function($scope, $resource){
+    var Drinks = $resource('/api/drinks');
+    Drinks.query(function(drinks){
+        $scope.drinks = drinks;
+    })
 }]);
