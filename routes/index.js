fc3a19526e476fcc36a9aea93f4fee9c6a210c3d
Dillon <Dillon@SaraAndrsonsMBP.home>
2016-01-23 11:42:07 -0500

    test out mongodb in index - should delete later

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('drinks');
+    collection.find({},function(e,docs){
+        //var json = JSON.stringify(docs, undefined, 2)
+        //var json_parsed = JSON.parse(docs);
+
+        var drinks = [];
+        docs.forEach(function(item, index) {
+            //console.log(item.name);
+            drinks.push(item.name);
+            //console.log("\n");
+        });
+
+        //console.log(json_parsed["56a290c55d08dd13939871d8"].name);
+        res.render('userlist', {
+            "drinks" : drinks
+        });
+    });
+});
+
 module.exports = router;
