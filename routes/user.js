2f4a8ba28967aae049d976082027ab69f063f043
Dillon <Dillon@Sara-Andersons-MacBook-Pro.local>
2016-01-29 21:45:51 -0500

    add put request to modify user's drink

--- routes/user.js
+++ routes/user.js
@@ -41,4 +41,39 @@ router.get('/:id', function(req, res) {
     }
 });
 
+// determines if the id passed as a parameter is valid for the database,
+// and that it is found in the database,
+// and then finally passes the ID on if its good,
+// otherwise returns an error in JSON
+router.put('/:id', function(req, res) {
+    var user_id = req.params.id;
+
+    if (isValidObjectID(user_id))
+    {
+        // look in the user database for key with id from URL parameter
+        var db = req.db;
+        var collection = db.get('users');
+        collection.findById(user_id, function(err,user){
+            if (err) throw err;
+            if (user !== null) {
+                var update = {
+                    "name": req.body.name,
+                    "recipe": req.body.recipe
+                };
+
+                collection.updateById(user_id, update, function(err) {
+                    if (err) throw err;
+                    res.json(user);
+                });
+            }
+            else {
+                res.json({"error" : "not_found"});
+            }
+        });
+    }
+    else {
+        res.json({"error" : "not_valid_objectId"});
+    }
+});
+
 module.exports = router;
