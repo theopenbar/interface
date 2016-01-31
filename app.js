7473ad2203402a0c6823649b47edc0a25530cbdb
Dillon <Dillon@Sara-Andersons-MacBook-Pro.local>
2016-01-30 19:09:58 -0500

    update db for write privileges

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 // for mongodb
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('mongodb://fakeuser:fakepassword@ds035004.mongolab.com:35004/heroku_ff1ms21p');
+var db = monk('mongodb://fakeuser2:fakeuser2@ds035004.mongolab.com:35004/heroku_ff1ms21p');
 
 var routes = require('./routes/index');
 var user = require('./routes/user');
