4075cdfa99bfc664a086e19cb30a27c682a61c80
Dillon <Dillon@SaraAndrsonsMBP.home>
2016-02-07 11:04:12 -0500

    get station details in add-drink

--- public/scripts/controllers/add-drink.controller.js
+++ public/scripts/controllers/add-drink.controller.js
@@ -1,5 +1,14 @@
-app.controller('AddDrinkCtrl', ['$scope', '$resource', 'drinksService',
-    function($scope, $resource, drinksService) {
+app.controller('AddDrinkCtrl', ['$scope', '$resource', '$location', 'stationService', 'drinksService',
+    function($scope, $resource, $location, stationService, drinksService) {
+        var url_params = $location.search();
+        var station_id = url_params.id;
+
+        // need station to get all available ingredients
+        var stationPromise = stationService.getStation(station_id);
+        stationPromise.then(function (station) {
+            $scope.station = station;
+        });
+
         $scope.save = function() {
             drinksService.saveDrink($scope.drink);
         };
