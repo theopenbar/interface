45d9530ac1f06e1ecc91a144d0a3086072f38e92
Dillon <Dillon@Sara-Andersons-MacBook-Pro.local>
2016-02-07 17:23:32 -0500

    valves start at 1, not 0

---
+++ public/scripts/filters/start-from.filter.js
@@ -0,0 +1,10 @@
+// http://stackoverflow.com/a/28411525
+app.filter('startFrom', function() {
+    return function(input, start) {
+        if(input) {
+            start = +start; //parse to int
+            return input.slice(start);
+        }
+        return [];
+    }
+});
