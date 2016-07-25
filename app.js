cec317e49a83e2fd9562186d5fae84218d5e8691
Dillon <ee.tinkerer@gmail.com>
2016-07-25 20:44:11 +0000

    add /api/station/ip route

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(function(req,res,next){
 app.use('/', routes);
 app.use('/api/user', user);
 app.use('/api/station', station);
+app.use('/api/station/ip', station);
 app.use('/api/drinks', drinks);
 app.use('/api/commander', commander);
 
