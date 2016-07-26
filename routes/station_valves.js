60795fa25096339690d879e06e3934a2d5259972
Dillon <ee.tinkerer@gmail.com>
2016-07-26 15:09:54 +0000

    mess with the ingredients array when changing # of valves

--- routes/station_valves.js
+++ routes/station_valves.js
@@ -5,14 +5,35 @@ var mongo = require('mongodb');
 router.post('/:id', function(req, res) {
     var db = req.db;
     var collection = db.get('stations');
+
+    // first, update the number of valves in the database
     collection.update({ "_id": mongo.ObjectID(req.params.id) },
         {$set:
             {num_valves: req.body.num_valves}
         },
         function(err, valves){
             if (err) throw err;
+            // probably shouldn't return here
             res.json(valves);
         });
+
+    // testing the ability to update ingredients array based  on num_valves
+    collection.findOne({ "_id": mongo.ObjectID(req.params.id) },function(err,station){
+        if (err) throw err;
+        if (station !== null) {
+            //console.log(data);
+            console.log(station.ingredients.length)
+
+            // if less ingredients than number of valves, add some
+            if (station.ingredients.length < station.num_valves) {
+                //collection.update()
+            }
+            // else, delete some
+            else {
+
+            }
+        }
+    });
 });
 
 module.exports = router;
