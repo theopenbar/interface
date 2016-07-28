b7351cc05d4071cab219e6ee76e4a4d68752a58c
Dillon <ee.tinkerer@gmail.com>
2016-07-28 15:09:58 +0000

    deal with python sending all values as strings

--- routes/station_ingredient.js
+++ routes/station_ingredient.js
@@ -2,16 +2,34 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb');
 
+// http://stackoverflow.com/a/21976486
+function isTrue(value){
+    if (typeof(value) == 'string'){
+        value = value.toLowerCase();
+    }
+    switch(value){
+        case true:
+        case "true":
+        case 1:
+        case "1":
+        case "on":
+        case "yes":
+            return true;
+        default:
+            return false;
+    }
+}
+
 router.post('/:id', function(req, res) {
     var db = req.db;
     var collection = db.get('stations');
     // edit item in ingredients array
-    collection.update({ "_id": mongo.ObjectID(req.params.id), "ingredients.valve":req.body.valve },
+    collection.update({ "_id": mongo.ObjectID(req.params.id), "ingredients.valve":parseInt(req.body.valve) },
         { $set:
             { 
                 "ingredients.$.type": req.body.type,
-                "ingredients.$.amount": req.body.amount,
-                "ingredients.$.pressurized": req.body.pressurized
+                "ingredients.$.amount": parseFloat(req.body.amount),
+                "ingredients.$.pressurized": isTrue(req.body.pressurized)
             }
         },
         function(err,station){
