840a5780fa2ae0f82a2574b914f5e385a517ca13
Dillon <ee.tinkerer@gmail.com>
2016-08-02 20:51:06 +0000

    add page access logging back in

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 // create app using express
@@ -22,6 +23,7 @@ app.use(function(req,res,next){
 
 // other things required for app
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
