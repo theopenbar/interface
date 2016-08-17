8af09765a0c15a16acfa59053aff97228db28a15
Dillon <ee.tinkerer@gmail.com>
2016-08-17 23:29:33 +0000

    remove favicon

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(function(req,res,next){
 });
 
 // other things required for app
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
