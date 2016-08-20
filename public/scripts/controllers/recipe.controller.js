93f0a3bb66ebf73161cc12de5815f8a583011629
Dillon <ee.tinkerer@gmail.com>
2016-08-20 01:37:29 +0000

    can add drinks and garnishes

--- public/scripts/controllers/recipe.controller.js
+++ public/scripts/controllers/recipe.controller.js
@@ -1,5 +1,19 @@
-app.controller('RecipeCtrl', ['$scope', 'recipeService',
-    function($scope, recipeService) {
+// http://www.shanidkv.com/blog/angularjs-adding-form-fields-dynamically
+app.controller('RecipeCtrl', ['$scope', '$compile', 'recipeService',
+    function($scope, $compile, recipeService) {
+
+        $scope.drinks = [];
+        $scope.garnishes = [];
+
+        $scope.addDrink = function(){
+            var newDrinkNum = $scope.drinks.length+1;
+            $scope.drinks.push({'id':'drink'+newDrinkNum});
+        };
+
+        $scope.addGarnish = function(){
+            var newGarnishNum = $scope.garnishes.length+1;
+            $scope.garnishes.push({'id':'garnish'+newGarnishNum});
+        };
 
         $scope.saveRecipe = function() {
             // send it to database
