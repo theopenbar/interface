aeba9821c48d53bb29899543293f35e8693f2cf8
Dillon <ee.tinkerer@gmail.com>
2016-08-23 19:55:19 +0000

    display Liquid on left side

--- public/scripts/controllers/recipe.controller.js
+++ public/scripts/controllers/recipe.controller.js
@@ -9,20 +9,25 @@ app.controller('RecipeCtrl', ['$scope', 'typeService', 'liquidService', 'recipeS
         });
 
         $scope.recipe = {name: null, liquids: [], garnishes: []};
+
+        // a fresh, blank liquid
         $scope.liquidEdit = false;
+        $scope.subtypes = false;
+        $scope.brands = false;
+        $scope.descriptions = false;
+
+        // a fresh, blank garnish
+        $scope.garnishEdit = false;
 
         $scope.addLiquid = function(){
-            $scope.liquidEdit = true;
+            $scope.liquidEdit = {"id": null, "amount": null, "requirement": true};
         };
 
         $scope.addGarnish = function(){
-            var newGarnishNum = $scope.recipe.garnishes.length+1;
-            $scope.recipe.garnishes.push(
-                {'id':newGarnishNum, name: null, amount: null}
-            );
+            $scope.garnishEdit = {"name": null, "amount": true};
         };
 
-        $scope.saveRecipe = function() {
+        $scope.submitRecipe = function() {
             // check that each form is filled in
             var recipe = $scope.recipe;
 
@@ -80,13 +85,7 @@ app.controller('RecipeCtrl', ['$scope', 'typeService', 'liquidService', 'recipeS
             });
         };
 
-        $scope.getSubtypes = function(drinkId) {
-            // get selected type from Drink id
-            // http://stackoverflow.com/a/7364203
-            var type = $scope.recipe.drinks.filter(function(v) {
-                return v.id === drinkId; // Filter out the appropriate one
-            })[0].type; // Get result and access the type property
-
+        $scope.getSubtypes = function(type) {
             // get all Subtypes from that Type
             var promise = typeService.getSubtypes(type);
             promise.then(function (subtypes) {
@@ -94,33 +93,34 @@ app.controller('RecipeCtrl', ['$scope', 'typeService', 'liquidService', 'recipeS
             });
         }
 
-        $scope.getBrands = function(drinkId) {
-             // get selected drink from Drink id
-            // http://stackoverflow.com/a/7364203
-            var drink = $scope.recipe.drinks.filter(function(v) {
-                return v.id === drinkId; // Filter out the appropriate one
-            })[0];
-
+        $scope.getBrands = function(subtype) {
             // get all Brands from that Type and Subtype
-            var query = {"type":drink.type, "subtype":drink.subtype};
+            var query = {"type":$scope.type, "subtype":subtype};
             var promise = liquidService.getBrands(query);
             promise.then(function (brands) {
                 $scope.brands = brands;
             });
         }
 
-        $scope.getDescription = function(drinkId) {
-             // get selected drink from Drink id
-            // http://stackoverflow.com/a/7364203
-            var drink = $scope.recipe.drinks.filter(function(v) {
-                return v.id === drinkId; // Filter out the appropriate one
-            })[0];
+        $scope.getDescription = function(brand) {
+            if (brand == "*Any") {
+                // get ID for "brand" : "*Any"
+                // http://stackoverflow.com/a/7364203
+                $scope.liquidEdit.id = $scope.brands.filter(function(v) {
+                    return v.brand === "*Any"; // Filter out the appropriate one
+                })[0]._id;
+            }
+            else {
+                // get all Descriptions from that Type and Subtype and Brand
+                var query = {"type":$scope.type, "subtype":$scope.subtype, "brand":brand};
+                var promise = liquidService.getDescriptions(query);
+                promise.then(function (descriptions) {
+                    $scope.descriptions = descriptions;
+                });
+            }
+        }
 
-            // get all Descriptions from that Type and Subtype and Brand
-            var query = {"type":drink.type, "subtype":drink.subtype, "brand":drink.brand};
-            var promise = liquidService.getDescriptions(query);
-            promise.then(function (descriptions) {
-                $scope.descriptions = descriptions;
-            });
+        $scope.addLiquidToRecipe = function() {
+            $scope.recipe.liquids.push($scope.liquidEdit);
         }
 }]);
