281f4b899df525f37305c8a858604b7434684183
Dillon <ee.tinkerer@gmail.com>
2016-08-25 21:02:58 +0000

    make create station API

--- routes/station.js
+++ routes/station.js
@@ -9,4 +9,19 @@ router.get('/:id', function(req, res) {
     })
 });
 
+// create a new document in stations and return it's information
+router.put('/', function(req, res) {
+    var station = new Station({
+        "host": "localhost",
+        "port": 8080,
+        "num_valves": 10
+    });
+
+    station.save(function (err, status) {
+        if (err) return (err);
+
+        res.json(status);
+    });
+});
+
 module.exports = router;
