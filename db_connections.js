48bd175948ccfb1f68bd66de3b5dc3c7d12decd7
Dillon <ee.tinkerer@gmail.com>
2016-08-25 19:16:09 +0000

    not using public or private databases yet

--- db_connections.js
+++ db_connections.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 var dbConns = {
-    pub: mongoose.createConnection('mongodb://localhost/publicTest',{
+    /*pub: mongoose.createConnection('mongodb://localhost/publicTest',{
         user: null,
         pass: null
     }),
     priv: mongoose.createConnection('mongodb://localhost/privateTest',{
         user: null,
         pass: null
-    }),
+    }),*/
     old: mongoose.createConnection('mongodb://ds035004.mongolab.com:35004/heroku_ff1ms21p',{
         user: 'fakeuser2',
         pass: 'fakeuser2'
